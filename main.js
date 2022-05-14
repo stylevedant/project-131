0d99e3930700498a75f948d0e39f241afdddee39
stylevedant <93001991+stylevedant@users.noreply.github.com>
2022-05-14 18:44:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+img = "";
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() 
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw() 
+{
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#00FF00");
+    rect(30, 60, 450, 350 );
+}
